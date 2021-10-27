b2bdcf1c954c83183c93432ae2b344405fb987b5
samuelagyemang2012 <khermz2012@gmail.com>
2021-10-27 14:04:33 +0800

    multimodel added

--- models/model.py
+++ models/model.py
@@ -1,4 +1,4 @@
-from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Flatten, Dense, Input, Dropout, MaxPooling2D, Conv2D, BatchNormalization, \
     Concatenate
 from tensorflow.keras.applications import ResNet50, InceptionV3, EfficientNetB4
@@ -35,6 +35,40 @@ def fully_connected(num_classes):
     return model
 
 
+def fully_connected2(concat, num_classes):
+    z = Flatten()(concat)
+    z = Dense(1024, activation='relu')(concat)
+    z = Dropout(0.2)(concat)
+    z = Dense(1024, activation='relu')(concat)
+    z = Dropout(0.2)(concat)
+    z = Dense(num_classes, activation='softmax')(concat)
+
+    return z
+
+def test_model():
+    inputA = Input(shape=(32,))
+    inputB = Input(shape=(128,))
+    # the first branch operates on the first input
+    x = Dense(8, activation="relu")(inputA)
+    x = Dense(4, activation="relu")(x)
+    x = Model(inputs=inputA, outputs=x)
+    # the second branch opreates on the second input
+    y = Dense(64, activation="relu")(inputB)
+    y = Dense(32, activation="relu")(y)
+    y = Dense(4, activation="relu")(y)
+    y = Model(inputs=inputB, outputs=y)
+    # combine the output of the two branches
+    combined = Concatenate(axis=1)([x.input, y.input])
+    # apply a FC layer and then a regression prediction on the
+    # combined outputs
+    z = Dense(2, activation="relu")(combined)
+    z = Dense(1, activation="linear")(z)
+    # our model will accept the inputs of the two branches and
+    # then output a single value
+    model = Model(inputs=[x.input, y.input], outputs=z)
+    return model
+
+
 def multi_model(input_tensor1, input_tensor2, input_shape1, input_shape2, num_classes, weights):
     base1 = efficient_net(input_tensor1, input_shape1, weights)
     base1._name = 'audio_net'
@@ -42,20 +76,16 @@ def multi_model(input_tensor1, input_tensor2, input_shape1, input_shape2, num_cl
     base2 = efficient_net(input_tensor2, input_shape2, weights)
     base2._name = 'image_net'
 
-    concat = Concatenate(axis=1)([base1.output, base2.output])
-    fc = fully_connected(num_classes)
+    x = base1(input_tensor1)
+    x = Model(input_tensor1, x)
 
-    model1 = Sequential()
-    model2 = Sequential()
-    model = Sequential()
+    y = base2(input_tensor1)
+    y = Model(input_tensor2, y)
 
-    m1 = model1.add(base1)
-    m2 = model2.add(base2)
+    concat = Concatenate([x.output, y.output])
+    fc = fully_connected2(concat, 4)
 
-    model.add(m1)
-    model.add(m2)
-    model.add(concat)
-    model.add(fc)
+    model = Model(input=[x.input, y.input], outputs=fc)
 
     return model
 
--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import cv2
 from models.model import *
+from keras.utils.vis_utils import plot_model
+
 
 path = "C:/Users/Administrator/Desktop/Sam/Multimodal_Fusion/training_data/new_spectograms/Parus-major-168221.png"
 img = cv2.imread(path)
@@ -7,6 +9,9 @@ img = cv2.imread(path)
 INPUT_SHAPE = (150, 150, 3)
 input_tensor = Input(shape=INPUT_SHAPE)
 
-model = multi_model(input_tensor, input_tensor, INPUT_SHAPE, INPUT_SHAPE, 4, 'imagenet')
+# model = multi_model(input_tensor, input_tensor, INPUT_SHAPE, INPUT_SHAPE, 4, 'imagenet')
+model = test_model()
+
 
 print(model.summary())
+plot_model(model, to_file='vis/model_plot.png', show_shapes=True, show_layer_names=True)
